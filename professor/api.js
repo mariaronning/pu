c679b3010045a7f66635f6d50f0efe26eba59bd7
Ingrid <ingrid.medalen@gmail.com>
2017-02-15 14:28:07 +0100

    fikset innlastning av data fra api, og søkefunksjon

--- professor/api.js
+++ professor/api.js
@@ -1,7 +1,19 @@
+var btn = document.getElementById("btn");
 
-function httpget()
-var url = "http://www.ime.ntnu.no/api/course/en/" + "tdt4145";
-var course = new XMLHttpRequest();
-course.open("GET",url, false)
-course.send(null); 
-return course.responseText; 
+btn.addEventListener("click", e =>{
+	
+var request = new XMLHttpRequest();
+request.open("GET","http://www.ime.ntnu.no/api/course/-");
+request.onload = function(){
+	var courses = JSON.parse(request.responseText); 
+	console.log(courses);
+};
+
+request.send(); 
+});
+
+
+function myFunction() {
+    var x = document.getElementById("sok");
+    console.log("You are searching for: " + x.value);
+}
