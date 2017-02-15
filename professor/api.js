b133a339cbd58964b16e993891ca83a826138098
Ingrid <ingrid.medalen@gmail.com>
2017-02-15 16:41:15 +0100

    prøver å få lagt til fag du søker på som displayes, bruker jason objektene

--- professor/api.js
+++ professor/api.js
@@ -1,4 +1,22 @@
 var btn = document.getElementById("btn");
+var x = document.getElementById("sok");
+var info = document.getElementById("info");
+function myFunction() {
+    console.log("You are searching for: " + x.value);
+	
+}
+
+function renderHTML(data){
+	var htmlstring ="";
+	for(i=0; i< data.length; i++){
+		htmlstring += "<p>" + data.course[i].name + data[i].course.code +"</p>";
+		console.log(data);
+	}
+	info.innerHTML = htmlstring;
+	
+}
+	
+
 
 btn.addEventListener("click", e =>{
 	
@@ -7,13 +25,10 @@ request.open("GET","http://www.ime.ntnu.no/api/course/-");
 request.onload = function(){
 	var courses = JSON.parse(request.responseText); 
 	console.log(courses);
+	renderHTML(courses);
 };
 
 request.send(); 
 });
 
 
-function myFunction() {
-    var x = document.getElementById("sok");
-    console.log("You are searching for: " + x.value);
-}
