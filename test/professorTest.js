371c55db16a44a01491939dfeddbc2fee5acac41
mayhelenrs <mayhelenrs@icloud.com>
2017-02-24 12:18:34 +0100

    Trying  protractor test

--- test/professorTest.js
+++ test/professorTest.js
@@ -1,18 +1,8 @@
-'use strict'
+// spec.js
+describe('Protractor Demo App', function() {
+  it('should have a title', function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
 
-var chai = require("chai");
-var expect = chai.expect;
-//var professor = require('../professor/professor.js');
-//var professor_html = require('../professor/professor.html');
-chai.should();
-
-
-function checkFiresearch(subject) {
-    return fireSearch(subject, 1);
-}
-
-describe('testing if fireSearch returns usable values', function(){
-    it('should return true if answers are equal', function(){
-        checkFiresearch('TDT4145').should.equal('TDT4145');
-    });
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
 });
