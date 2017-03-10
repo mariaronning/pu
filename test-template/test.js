c2a3f1c90c1a8c9792e5cc0edd428ba3ea324c8c
Ingrid <ingrid.medalen@gmail.com>
2017-03-10 14:48:30 +0100

    Fikset css og gjort klart for innhenting fra database

--- test-template/test.js
+++ test-template/test.js
@@ -1,13 +1,14 @@
-var database = firebase.database();
+var database = firebase.database().ref().child('Courses/');
 
-
+function firesearch(value){
+	
 var view = {
 	name : "Datamodellering og databasesystemer",
 	code : "TDT4145"
     };
 
     function loadtemp(){
-        var output = Mustache.render("{{name}} -  {{code}}", 			view);
+        var output = Mustache.render("{{name}} -  {{code}}", view);
         document.getElementById('subject').innerHTML = output;
       }
     
